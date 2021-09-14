68bd7aae9b10c94ca5c6f8503903760c14eec47f
BADBOY.019 <forlan_vega@hotmail.com>
2021-09-14 16:08:17 -0500

    14-9-21--4:08

--- universidad/settings.py
+++ universidad/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -125,4 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/universidad/Academico/static/"
+STATIC_ROOT = "/var/www/dgv2019_pythonanywhere_com/static/"
